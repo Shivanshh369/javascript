f419af1add56fea98321833bf80a5c7e443e3c39
Shivanshh369 <shivanshhh369@gmail.com>
2024-03-14 19:24:00 +0530

    datesinjs

--- 01_basics/06_num_and_maths.js
+++ 01_basics/06_num_and_maths.js
@@ -38,3 +38,5 @@ console.log(Math.floor(Math.random() * (max - min + 1)) + min);
 
 
 
+
+
---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,12 @@
+//dates
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
+
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2024, 0, 23)
+console.log(myCreatedDate.toDateString());
