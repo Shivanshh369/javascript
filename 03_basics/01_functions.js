2e74d17de53e466808d8e42129f069acab908909
Shivanshh369 <shivanshhh369@gmail.com>
2024-04-04 00:08:58 +0530

    updated functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -50,3 +50,36 @@ function loginUserMessage(username = "sam") {
 // console.log(loginUserMessage("shiv"));
 console.log(loginUserMessage("shivansh"));
 
+
+function calculateCartPrice(...num1) {
+    return num1
+}
+
+//console.log(calculateCartPrice(200, 400, 500));
+const user = {
+    username: "shivansh",
+    price: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function retrunSecondValue(getArray) {
+    return getArray[1]
+}
+
+//console.log(retrunSecondValue(myNewArray));
+    
+console.log(retrunSecondValue([200, 400, 500, 1000]));
