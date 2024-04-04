c1d6a6b7266d64c3435ce48384e86e0c6328a656
Shivanshh369 <shivanshhh369@gmail.com>
2024-04-04 18:11:25 +0530

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,66 @@
+//var c=300
+// let a =300
+// if (true) {
+//     let a = 10
+//     const b = 20
+//    // console.log("INNER:", a);
+    
+// }
+
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
+
+
+// function one() {
+//     const username = "shivansh"
+
+//     function two() {
+//         const website = "youtube"
+//       //  console.log(username);        
+//     }
+    
+//     //console.log(username);
+
+//    // two()
+// }
+
+
+// //one()
+
+
+if (true) {
+    const username = "Shivansh";
+    if (username === "Shivansh") {
+          const website = " Mishra";    
+          //console.log(username + website);
+    
+    }
+    //console.log(website);  error
+}
+
+//console.log(username);
+
+
+//----------------------interesting---------------
+
+
+console.log (addone(5))
+function  addone(num) {
+       return num + 1    
+}
+
+
+//addTwo(5)
+
+const addTwo = function (num) {
+    return num + 2
+    
+}
+
+
