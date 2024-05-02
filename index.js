d6a47c7686db35da2256fef5ff2ff05a3d9524b8
Shivanshh369 <shivanshhh369@gmail.com>
2024-05-02 19:06:37 +0530

    update

---
+++ index.js
@@ -0,0 +1,20 @@
+let language = "bihari";
+
+if (language === "english") {
+    console.log("Hello");
+} 
+else if (language === "spanish") {
+    console.log("Hola");
+}
+else if (language === "bihari") {
+    console.log("kya haal maharaj");
+}
+ else if (language === "German") {
+    console.log("Hallo");
+}
+ else if (language === "Hindi") {
+    console.log("Namaste");
+} 
+else {
+    console.log('Language not supported');
+}
